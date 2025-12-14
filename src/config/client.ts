57871ed9ce92fe26d9328b214ee7eaba01574048
Dmitriy Pechenyuk <dmitriypechenyuk0@gmail.com>
2025-12-14 20:50:14 +0200

    remove error with desynchronization of .db

--- src/config/client.ts
+++ src/config/client.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '../generated/prisma';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 const adapter = new PrismaBetterSqlite3({
-  url: "file:../database.db"
+  url: "file:./database.db"
 })
 
 export const PRISMA_CLIENT = new PrismaClient({
