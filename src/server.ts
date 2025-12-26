b08664eb1fa25a7da42dcf1204c625f61329a31a
Dmitriy Pechenyuk <dmitriypechenyuk0@gmail.com>
2025-12-26 04:53:43 +0200

    swagger-ui 1.0.0

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app: express.Express = express()
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT: number = 8000
 const HOST: string = 'localhost'
